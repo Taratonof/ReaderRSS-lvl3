02d7472fdc4a3502c62ade2adf55fbdd843ec144
Artur <taratonofa@yandex.ru>
2019-09-16 22:47:41 +0300

    add input URL and validation URL

--- src/init.js
+++ src/init.js
@@ -1,8 +1,34 @@
 import '@babel/polyfill';
-import Example from './Example';
+import validator from 'validator';
+
+const render = (state) => {
+  const input = document.getElementById('formGroupExampleInput');
+  if (state.registrationProcess.valid) {
+    input.style.border = null;
+  } else {
+    input.style.border = "thick solid red";
+  }
+};
+
+const app = () => {
+  const state = {
+    registrationProcess: {
+      valid: true,
+    },
+  };
+  const input = document.getElementById('formGroupExampleInput');
+  input.addEventListener('keyup', () => {
+    if (validator.isURL(input.value)) {
+      alert('YEEEEE');
+      state.registrationProcess.valid = true;
+    } else {
+      alert('NOOO');
+      state.registrationProcess.valid = false;
+    }
+  });
+  render(state);
+};
 
 export default () => {
-  const element = document.getElementById('point');
-  const obj = new Example(element);
-  obj.init();
+  app();
 };
