c8e7c25e63d159f631f9e17db032f150b073f791
Artur <taratonofa@yandex.ru>
2019-09-23 22:32:17 +0300

    add listNews

--- src/init.js
+++ src/init.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill';
 import validator from 'validator';
 import axios from 'axios';
+import WatchJS from 'melanke-watchjs';
 
 const render = (state) => {
   const input = document.getElementById('formGroupExampleInput');
@@ -14,6 +15,7 @@ const render = (state) => {
   state.listChannelState.forEach((elem) => {
     const listChannelElem = document.createElement('a');
     listChannelElem.setAttribute('href', '#');
+    listChannelElem.setAttribute('url', elem.url);
     listChannelElem.classList.add('list-group-item', 'list-group-item-action');
     const div = document.createElement('div');
     div.classList.add('d-flex', 'w-100', 'justify-content-between');
@@ -28,7 +30,6 @@ const render = (state) => {
     listChannelElem.append(p);
     listChannel.append(listChannelElem);
   });
-  console.log(state.listChannelState);
 };
 
 const app = () => {
@@ -37,6 +38,11 @@ const app = () => {
       valid: true,
     },
     listChannelState: [],
+    itemChannel: [],
+    menu: {
+      click: false,
+      target: null,
+    },
   };
   const input = document.getElementById('formGroupExampleInput');
   input.addEventListener('keyup', () => {
@@ -49,6 +55,42 @@ const app = () => {
     }
   });
 
+  const listChannel = document.getElementById('ggg');
+  WatchJS.watch(state, 'menu', () => {
+    if (!state.menu) {
+      return;
+    }
+    const beforeActiveButton = listChannel.querySelector('.active');
+    if (beforeActiveButton !== null) {
+      beforeActiveButton.classList.remove('active');
+    }
+    state.menu.target.classList.add('active');
+    state.menu.click = false;
+  });
+  WatchJS.watch(state, 'itemChannel', () => {
+    const listNews = document.getElementById('listNews');
+    listNews.innerHTML = '';
+    state.itemChannel.forEach((elem) => {
+      const news = document.createElement('li');
+      news.classList.add('list-group-item');
+      news.textContent = elem.querySelector('title').textContent;
+      listNews.append(news);
+    });
+  });
+
+  listChannel.addEventListener('click', (e) => {
+    state.menu.click = true;
+    const targetButtonChanel = e.target.closest('.list-group-item-action');
+    state.menu.target = targetButtonChanel;
+    axios.get(targetButtonChanel.getAttribute('url'))
+      .then((response) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(response.data, 'application/xml');
+        const items = [...doc.querySelectorAll('item')];
+        state.itemChannel = items;
+      });
+  });
+
   const button = document.querySelector('.btn-primary');
   button.addEventListener('click', () => {
     axios.get(`https://cors-anywhere.herokuapp.com/${input.value}`)
@@ -57,9 +99,9 @@ const app = () => {
         const doc = parser.parseFromString(response.data, 'application/xml');
         const title = doc.querySelector('title').textContent;
         const description = doc.querySelector('description').textContent;
-        state.listChannelState.push({ title, description });
+        const url = `https://cors-anywhere.herokuapp.com/${input.value}`;
+        state.listChannelState.push({ title, description, url });
         render(state);
-        //console.log(doc.querySelector('title').textContent);
       });
   });
 };
